08945722a5a62e8f017fb9a57e76082be7a8d9c2
HalilSacirovic <halilsacirovic03@gmail.com>
2023-02-08 20:59:31 +0100

    Updated alerbox

--- signup.js
+++ signup.js
@@ -1,54 +1,102 @@
-
-document.getElementById("signupbutton").addEventListener("click", function () {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
-    var fullName = document.getElementById("fullName").value;
-    var loginData = {
-      email: email,
-      password: password,
-      confirmPassword: confirmPassword,
-      fullName: fullName,
-    };
-    
-    
-    alertbox = document.getElementById("alertbox");
-    
-    
-    if(!email.includes("@") || password.length<6 || password.length>20)
-    {
-      document.getElementById("alertbox").style = "display:block"
-    }
-    
-    var xbutton = document.getElementById("xbutton").addEventListener("click",function()
-    {
-      document.getElementById("alertbox").style = "display:none";
-
-       document.getElementById("email").value= " ";
-       document.getElementById("password").value= "";
-       document.getElementById("confirmPassword").value= "";
-      document.getElementById("fullName").value= " ";
-
-      console.log(password);
-      console.log(confirmPassword);
-    })
+function Escape(){
   
-    fetch("https://js-course-server.onrender.com/user/signup", {
-      method: "POST",
-      body: JSON.stringify(loginData),
-      headers: {
-        "Content-Type": "application/json",
-      },
+  document.getElementById("email").value= " ";
+  document.getElementById("password").value= "";
+  document.getElementById("confirmPassword").value= "";
+ document.getElementById("fullName").value= " ";
+
+ console.log(password);
+ console.log(confirmPassword);
+
+ document.getElementById("alertbox").style = "display:none";
+
+
+}
+ ////////////////////////////////////////////////
+ ////////////////////////////////////////////////
+ ////////////////////////////////////////////////
+
+
+
+function SignUpWithEnter (){
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
+  var fullName = document.getElementById("fullName").value;
+  var loginData = {
+    email: email,
+    password: password,
+    confirmPassword: confirmPassword,
+    fullName: fullName,
+  };
+  
+  
+  alertbox = document.getElementById("alertbox");
+  
+  
+  if(!email.includes("@") || password.length<6 || password.length>20)
+  {
+    document.getElementById("alertbox").style = "display:block";
+
+    document.addEventListener("keydown", function(event) {
+      if (event.key === "Escape") {
+        Escape();
+      }
+    });
+  }
+  
+
+   ////////////////////////////////////////////////
+ ////////////////////////////////////////////////
+ ////////////////////////////////////////////////
+
+  document.getElementById("xbutton").addEventListener("click",function()
+  {
+    Escape();
+ 
+  })
+
+   ////////////////////////////////////////////////
+ ////////////////////////////////////////////////
+ ////////////////////////////////////////////////
+
+
+  fetch("https://js-course-server.onrender.com/user/signup", {
+    method: "POST",
+    body: JSON.stringify(loginData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
     })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        if (data.userId) {
-          // uspesno
-          alert("Uspesna regisracija");
-          window.location.href = "login.html";
-        } 
-        
-      });
+    .then(function (data) {
+      if (data.userId) {
+        // uspesno
+        alert("Uspesna regisracija");
+        window.location.href = "login.html";
+      } 
+      
+    });
+}
+
+
+
+ ////////////////////////////////////////////////
+ ////////////////////////////////////////////////
+ ////////////////////////////////////////////////
+
+
+ document.getElementById("signupbutton").addEventListener("click", function () {
+      SignUpWithEnter();
   });
+
+  
+
+  document.getElementById("confirmPassword").addEventListener("keydown",function(event){
+    if (event.key === "Enter") {
+      SignUpWithEnter();
+    }
+  })
+
