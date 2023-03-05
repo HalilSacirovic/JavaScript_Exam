f04c876af735a457760da0cdfdfbafbae2eb1c43
HalilSacirovic <halilsacirovic03@gmail.com>
2023-03-05 10:47:15 +0100

    Pitacu Profesora

--- App_Project/mainscript.js
+++ App_Project/mainscript.js
@@ -1,39 +1,74 @@
-const item = [
-  {
-    id: "1",
-    question: "Dobro si?",
-    answer: ["Elhamdulillah", "Dobro sam", "Very GUT"],
-    correct: "Elhamdulillah",
-    points: 5,
-  },
-  {
-    id: "2",
-    question: "Glavni Grad Tutina ",
-    answer: ["Ribarice", "Tutin je Grad", "Ne znam"],
-    correct: "Tutin je Grad",
-    points: 8,
-  },
-  {
-    id: "3",
-    question: "Koliko je sati",
-    answer: ["Kolko ti treba", "Jos malo pa sace", "Isto ko juce"],
-    correct: "Jos malo pa sace",
-    points: 4,
-  },
-  {
-    id: "4",
-    question: "Ne znam ni ja sam",
-    answer: ["Isto", "Takode", "Ne znam"],
-    correct: "Ne znam",
-    points: 3,
-  },
-];
+
+const megaArray = [
+  item = [
+    {
+      id: "1",
+      question: "Dobro si?",
+      answer: ["Elhamdulillah", "Dobro sam", "Very GUT"],
+      correct: "Elhamdulillah",
+      points: 5,
+    },
+    {
+      id: "2",
+      question: "Glavni Grad Tutina ",
+      answer: ["Ribarice", "Tutin je Grad", "Ne znam"],
+      correct: "Tutin je Grad",
+      points: 8,
+    },
+    {
+      id: "3",
+      question: "Koliko je sati",
+      answer: ["Kolko ti treba", "Jos malo pa sace", "Isto ko juce"],
+      correct: "Jos malo pa sace",
+      points: 4,
+    },
+    {
+      id: "4",
+      question: "Ne znam ni ja sam",
+      answer: ["Isto", "Takode", "Ne znam"],
+      correct: "Ne znam",
+      points: 3,
+    },
+  ],
+
+ item = [
+    {
+      id: "5",
+      question: "Ko je napisao na Drini Cuprija",
+      answer: ["Ivo Andric", "Nisam", "Ne znam"],
+      correct: "Ivo Andric",
+      points: 5,
+    },
+    {
+      id: "6",
+      question: "Glavni Grad Kanade",
+      answer: ["Novi Pazar", "Otava", "Sjenica"],
+      correct: "Otava",
+      points: 8,
+    },
+    {
+      id: "7",
+      question: "Koliko dan ima minuta",
+      answer: ["1440", "2569", "69125"],
+      correct: "1440",
+      points: 4,
+    },
+    {
+      id: "8",
+      question: "Za koliko ces preci 100 km ako se kreces 100km/h",
+      answer: ["2h", "30min", "1h"],
+      correct: "1h",
+      points: 3,
+    },
+  ]
+]
 
 var placeForAnswers = document.getElementById("answers");
 var itemquestion = document.getElementById("question");
 var answeritem = document.getElementById("1");
 
 let selectedQuestion;
+let megaSelectedQuestion;
 let indexpitanja = 0;
 let bodovi = 0;
 
